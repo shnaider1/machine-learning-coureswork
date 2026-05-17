bc970a7c9119776e8c7be26c83be3551a39bf622
tameem <dzl510@york.ac.uk>
2026-05-17 06:21:07 +0100

    baseline CNN

--- train.py
+++ train.py
@@ -1,8 +1,45 @@
 # train.py
 
+import torch
+from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
+
+class PetCNN(nn.Module):
+    def __init__(self, num_classes=37):
+        super().__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(128 * 28 * 28, 256),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(256, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Using device:", device)
 
 image_transform = transforms.Compose([
     transforms.Resize((224, 224)),
@@ -28,4 +65,7 @@ train_loader = DataLoader(
 )
 
 images, labels = next(iter(train_loader))
+images = images.to(device)
 
+model = PetCNN(num_classes=37).to(device)
+outputs = model(images)
